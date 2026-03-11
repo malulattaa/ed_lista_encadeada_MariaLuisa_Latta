2b1a7812a7ac70774e7db7ca229101ce4463db5f
malulattaa <maria.latta2@estudante.ifms.edu.br>
2026-03-10 23:10:11 -0400

    Inserindo e buscando elementos

--- ex.py
+++ ex.py
@@ -1,13 +1,40 @@
+
+
 class Node:
-    def _init_(self, data) -> None:
-        self.data = data
+    def __init__(self, value) -> None:
+        self.value = value
         self.next = None
 
 class LinkedList:
-    def _init_(self):
+    def __init__(self):
         self.head = None
+        self.size = 0
         
-    def insert_beginning(self, data) -> None:
-        novo_node = Node(data)
+    def insert_end(self, value):
+        if self.head:
+            pointer = self.head
+            while(pointer.next):
+                pointer = pointer.next
+            pointer.next = Node(value)
+            
+        else:
+            self.head = Node(value)
+        self.size = self.size + 1
         
-        
+    def __len__(self):
+        return self.size
+    
+    def search(self, value):
+        pointer = self.head
+        i = 0
+        while (pointer):
+            if pointer.data == value:
+                return i
+            pointer = pointer.next
+            i = i + 1
+        return None
+    # ou raise ValueError ("O elemento não está na lista")
+
+lista = LinkedList()
+lista.insert_end(7)
+len(lista)
